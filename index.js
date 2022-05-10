e9f96c251de1e38c79cab2d78ed136db94036bd1
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2022-05-10 12:34:49 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const poke_container = document.getElementById('poke_container');
-const pokemons_number = 150;
+const poke_container = document.getElementById('poke-container')
+const pokemon_count = 150
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -15,62 +15,50 @@ const colors = {
     flying: '#F5F5F5',
     fighting: '#E6E0D4',
     normal: '#F5F5F5'
-};
-const main_types = Object.keys(colors);
-
-const fetchPokemons = async () => {
-    for (let i = 1; i <= pokemons_number; i++) {
-        await getPokemon(i);
-    }
-};
-
-const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const res = await fetch(url);
-    const pokemon = await res.json();
-    createPokemonCard(pokemon);
-};
-
-function createPokemonCard(pokemon) {
-    const pokemonEl = document.createElement('div');
-    pokemonEl.classList.add('pokemon');
-
-    const poke_types = pokemon.types.map(type => type.type.name);
-    const type = main_types.find(type => poke_types.indexOf(type) > -1);
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-    const color = colors[type];
-
-    pokemonEl.style.backgroundColor = color;
-
-    const pokeInnerHTML = `
-        <div class="img-container">
-        <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="${name}" />
-        </div>
-        <div class="info">
-            <span class="number">#${pokemon.id
-            .toString()
-            .padStart(3, '0')}</span>
-            <h3 class="name">${name}</h3>
-            <small class="type">Type: <span>${type}</span></small>
-        </div>
-    `;
-
-    pokemonEl.innerHTML = pokeInnerHTML;
-
-    poke_container.appendChild(pokemonEl);
 }
 
-fetchPokemons();
+const main_types = Object.keys(colors)
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+}
 
-floating_btn.addEventListener('click', () => {
-    social_panel_container.classList.toggle('visible')
-});
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonCard(data)
+}
 
-close_btn.addEventListener('click', () => {
-    social_panel_container.classList.remove('visible')
-});
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
+    </div>
+    `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokemonEl)
+}
+
+fetchPokemons()
